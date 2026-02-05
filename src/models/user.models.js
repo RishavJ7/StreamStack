808b8551431bc3d8bb7610c2c5bb9a7b5c99127d
Rishav Raj <rishav.2004th@gmail.com>
2026-02-05 12:55:00 +0530

    controllers: implement comment add, edit, delete and paginated listing

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -32,7 +32,7 @@ const userSchema = new Schema({
     coverImage: {
         type: String, //cloudinary Url
     },
-    wathHistory: [
+    watchHistory: [
         {
             type: Schema.Types.ObjectId,
             ref: "Video"
