25c8d46c5ddaa2af9186f4cc1e8871cd8389c53f
Rishav Raj <rishav.2004th@gmail.com>
2026-02-07 13:00:00 +0530

    fix: patch watch-history aggregation and cover-image update logic

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,5 +1,21 @@
 import { Router } from "express";
+import { verifyJWT } from "../middleware/getUser.middleware.js"
+import {addVideoToPlaylist, createPlaylist, deletePlaylist, getPlaylistById, getUserPlaylists, removeVideoFromPlaylist, updatePlaylist} from "../controllers/playlist.controller.js"
 
 const router = Router()
+
+router.route("/create-playlist").post(createPlaylist)
+
+router.route("/update-playlist/:playlistId").post(verifyJWT,updatePlaylist)
+
+router.route("/delete-playlist/:playlistId").get(verifyJWT,deletePlaylist)
+
+router.route("/get-playlist/:playlistId").get(verifyJWT,getPlaylistById)
+
+router.route("/get-User-playlist/:userId").get(verifyJWT,getUserPlaylists)
+
+router.route("/add-video-playlist/:playlistId/:videoId").post(verifyJWT,addVideoToPlaylist)
+
+router.route("/remove-video-playlist:/playlistId/:videoId").post(verifyJWT,removeVideoFromPlaylist)
 
 export default router
