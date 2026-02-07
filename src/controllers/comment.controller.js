0db174a85d319b79c4e9c300c40a85d02be80eab
Rishav Raj <rishav.2004th@gmail.com>
2026-02-07 11:10:00 +0530

    app: configure CORS, cookie-parser, JSON body limit and static file serving

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import {Comment} from "../models/comment.models.js"
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
+import { createComment, deleteComment, getAllCommentVideo, updateComment } from "../controllers/comment.controller.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router()
+
+router.route("/add-comment").post(createComment)
+
+router.route("/update-comment/:commentId").post(verifyJWT,updateComment)
+
+router.route("/delete-comment/:commentId").get(verifyJWT,deleteComment)
+
+router.route("/get-all-video-comments/:videoId").get(verifyJWT,getAllCommentVideo)
 
 export default router
